e1cc09dc415011de0e42c0821900645c820611c5
진태만 <tamario@naver.com>
2023-07-22 09:38:53 +0900

    Add to Python Work space[2023.07.22 09:38(T_01), by 진태만]

--- 52_OB_work_local/ob_sql_fm_copy.py
+++ 52_OB_work_local/ob_sql_fm_copy.py
@@ -5,7 +5,8 @@ import os	# 운영체제(OS) 제어 Lib
 import sys	# 파이씬 인터프리터 제어 Lib
 import csv	# CSV 파일 Lib 
 import shutil      # 고수준 파일 연산 Lib
-import datetime     # 시간 Lib
+import datetime     # 날짜, 시간 Lib
+import time     	# 시간 Lib
 import subprocess	# 하위 프로세스 관리 표준 Lib
 
 src_path = ""	# 복사할 파일 경로
@@ -49,7 +50,7 @@ f_close()
 csv_res = list()   # csv_res 설정 
 print("\n\n\n\n[@_TT] ■■■ [/ob_sql_fm_copy.py] ==> [T_01] ■■■■■■ [######################### [OB sql 파일 복사 쳐리 Start] #########################] ■■■■■■ ")
 
-ob.sort = 0       # OB 처리 종류【1. 가!발 테스트 추출. 2.  CRM 데이다 추출)	■■■■■■■■■■■■■■■■■■■■■■■■
+ob_sort = 0       # OB 처리 종류【1. 가!발 테스트 추출. 2.  CRM 데이다 추출)	■■■■■■■■■■■■■■■■■■■■■■■■
 
 if len(sys.argv) < 2 -      # 인자값이 없으연
 	ob_sort = 1
@@ -67,7 +68,8 @@ else :
 print("\n\n\n\n[@_T] ■■ [/ob_sql_fm_copy.py] ==> [T_021 [ob_sort]"+ str(ob_sort) +"[ob_dsgn 폴더】"+ str(ob_folder) )
 
 result_title = list()   # 제목 설정
-now_date = time.strftime("%Y-%m-%d %H:%M:%S")		# 오늘 날짜
+now_date = time.strftime("%Y-%m-%d %H:%M:%S")	# 오늘 날짜
+now_ydmhm = time.strftime("%Y.%m.%d %H:%M")		# 오늘 날짜(년.월.일 시:분)
 result_title.append("CSV File Info[Start Date "+ str(now_date) +"]")	# 제목 설정
 csv_res.append(result_title)
 
@@ -78,15 +80,18 @@ result_head .append("Copy File path")    # 02.복사할 파일 경로
 result_head.append("Copied File path")    # 03. 복사된 목적지 파일 경로 
 csv_res .append(result_head)   # HEAD 설정
 
-strNo_Str = None        # No
-cmt_No = "06"    # 커밋 메세지 번호 ■■■■■■■■■■■ 
+strNo_Str = None        # No]
+firtNo = "" 
+lastNo = ""
+intNo = 0
 
 command = 'git pull origin develop'    # 0. 원격 저장소 데이다 가저 오기
-proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdoul
+proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
 out_cmd = proc.read()
-print("\n\n [@_T] ■■■ [/ob_sqljm_copy.py] ==> [T_11] [0. 원격 저장소 데이다 가저 오기(git pull)]"+ out_cmd decode('utf-8') ) 
+print("\n\n [@_T] ■■■ [/ob_sql_fm_copy.py] ==> [T_11] [0. 원격 저장소 데이다 가저 오기(git pull)]"+ out_cmd decode('utf-8') ) 
 
-for i in table_list:		# i ~ table_list
+
+stdoutfor i in table_list:		# i ~ table_list
 	print("\n\n[@_T] ■ [/ob_sql_fm_copy.py] ==> [T_50] [table_list] [i_번째]"+ i +"[01. 파일 명]"+ crm_file_nm  +"[CSV 파일 정보 확인 작업 중...] .....■■■■■■ ")
 
 	try :
@@ -103,7 +108,7 @@ for i in table_list:		# i ~ table_list
 
 	result = list()     # 결과 list 설정
 
-	if ob.sort == 1 :    # 0B 쳐리 종휴가 1 이면(1: 개발 테스트 추출)
+	if ob_sort == 1 :    # 0B 쳐리 종휴가 1 이면(1: 개발 테스트 추출)
 		num_file_nm = num_file_nm
 	else :	 # 처리 종류가 2 이면(2: CRM 데이다 추출) 
 		num_file_nm = num_file_nm +"_CRMT"
@@ -139,36 +144,42 @@ for i in table_list:		# i ~ table_list
 	if strNo_Str != None and strNo_Str =! ''        # No .가 존재하연 
 		strNo_Str = "No. "+ strNo_Str +"No. "+ strNo
 	else :
-		strNo_Str = "NNo. "+ strNo
- 	print("\n\n [@_T] ■■■ [/ob_sqljm_copy.py] ==> [T_60] [파일 복사 처리 End] ■■■■■■■■■■ \n\n\n\n\n\n")
+		strNo_Str = "NNo. "+ str(strNo_Str)
+		firtNo = str(intNo)
+	
+	intNo = intNo + 1
+	lastNo = str(intNo)
+	print("\n\n [@_T] ■■■ [/ob_sql_fm_copy.py] ==> [T_60] [파일 복사 처리 End] ■■■■■■■■■■ \n\n\n\n\n\n")
 
-print("\n\n [@_T] ■■■ [/ob_sqljm_copy.py] ==> [T_61] [Git 경보 처려 시작.......@@@@@ ■■■■■■■■■■ (strNo_Str "+ str(strNo_Str)  +"\n\n\n\n\n\n")
+print("\n\n [@_T] ■■■ [/ob_sql_fm_copy.py] ==> [T_61] [Git 경보 처려 시작.......@@@@@ ■■■■■■■■■■ (strNo_Str "+ str(strNo_Str) +"\n\n\n\n\n\n")
 
-time.sleep(5)     # 5초 기다영
+time.sleep(2)     # 2초 기다영
 
 command = 'git status'    # 0. Git 저장소의 상태 확인
-proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdoul
+proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
 out_cmd_status = proc.read()
-print("\n\n [@_T] ■■■ [/ob_sqljm_copy.py] ==> [T_63_0] [0. Git 저장소의 상태 확인(git status)]"+ out_cmd_status decode('utf-8') ) 
+print("\n\n [@_T] ■■■ [/ob_sql_fm_copy.py] ==> [T_63_0] [0. Git 저장소의 상태 확인(git status)]"+ out_cmd_status.decode('utf-8') ) 
 
 command = 'git add -A'    # 3. Git 스테이지 영역에 추가
-proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdoul
-out_cmd_add = proc_read()
-print("\n\n [@_T] ■■■ [/ob_sqljm_copy.py] ==> [T_63_1] [3. Git 스테이지 영역에 추가(git add)]"+ out_cmd_add.decode( utf-8 )) 
+proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+out_cmd_add = proc.read()
+print("\n\n [@_T] ■■■ [/ob_sql_fm_copy.py] ==> [T_63_1] [3. Git 스테이지 영역에 추가(git add)]"+ out_cmd_add.decode('utf-8') )
 
-cmt_No = "10"    # 깃 메세지 번호 ■■■■■■■■■■■
-cmd_cmt_msg = 'git commit -m "Add to TEST 데이타 추출('+ datetime.datetime.now().strftime('%y.%m.%d') +'_'+ cmt_No +') [('+ str(strNo_Str) +'), by 진태만]"'   # 4. Git 로컬 저장소 영역에 커밋 처리
+if intNo > 10 # No.가 10보다 크면
+	strNo_Str = "No. "+ str(intfirtNoNo) + " ~ No. "+ str(lastNo)
+
+cmd_cmt_msg = 'git commit -m "Add to '+ ob_dsgn_sort_nm +' 데이타 추출('+ now_ydmhm +') [('+ str(strNo_Str) +'), by 진태만]"'   # 4. Git 로컬 저장소 영역에 커밋 처리
 # git commit -m 'Add to TEST 대이다 추출(22.07.20J)1》[(No  31, No. 32) by 진태만1 
-proc = ubprocess.Popen(cmd_cmt_msg, shell=True, stdout=subprocess.PIPE).stdoul
+proc = subprocess.Popen(cmd_cmt_msg, shell=True, stdout=subprocess.PIPE).stdout
 out_cmt_msg = proc.read()
-print("\n\n [@_T] ■■■ [/ob_sqljm_copy.py] ==> [T_63_2] [4. Git 로걸 저장소 영역에 추가(커잇 에세지)J  ■■■■■■■■■■■■ "+ str(cmd_cmt_msg))
-print("\n\n [@_T] ■■■ [/ob_sqljm_copy.py] ==> [T_63_2] [4  Git 로걸 저장소 영역에 추가(git commit)] ■■■■■■■■■■■■ "* out_cmt_msg.decode( utf-8)) 
+print("\n\n [@_T] ■■■ [/ob_sql_fm_copy.py] ==> [T_63_2] [4. Git 로걸 저장소 영역에 추가(커잇 에세지)J  ■■■■■■■■■■■■ "+ str(cmd_cmt_msg))
+print("\n\n [@_T] ■■■ [/ob_sql_fm_copy.py] ==> [T_63_2] [4  Git 로걸 저장소 영역에 추가(git commit)] ■■■■■■■■■■■■ "* out_cmt_msg.decode( utf-8)) 
 
-command = 'git push -f origin develop'      # 5. 원격  저장소에 반영
-proc = ubprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdoul
+command = 'git push origin develop'      # 5. 원격  저장소에 반영
+proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
 out_cmd_push = proc.read()
-print("\n[@_T] ■■ [/ob_sql_fm_copy py] ==> [T_63_3] [5. 원격 方!장소에 반영(git push)]"+ out_cmd_push.decode('utf-8'))
-print("\n\n [@_T] ■■■ [/ob_sqlJm.copy py] ==> [T_80 [Git 정보 쳐리 시작 End] ■■■ @@@@@ ■■■■■■■■■ ■\n\n\n\n\n\n")
+print("\n[@_T] ■■ [/ob_sql_fm_copy.py] ==> [T_63_3] [5. 원격 方!장소에 반영(git push)]"+ out_cmd_push.decode('utf-8'))
+print("\n\n [@_T] ■■■ [/ob_sql_fm_copy.py] ==> [T_80 [Git 정보 쳐리 시작 End] ■■■ @@@@@ ■■■■■■■■■ ■\n\n\n\n\n\n")
 
 with open(res_path, 'w') as file:	    # data 디렉토리안에 res_path 경로의 파일을 생성	
 	write = csv.writer(file)    # 쓰고 싶은 내용 입력
