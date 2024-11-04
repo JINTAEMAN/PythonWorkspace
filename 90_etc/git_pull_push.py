bdecf7e4acaa335f46d86e0430b5a78355529d27
tamario <tamario@naver.com>
2024-11-05 07:52:43 +0900

    ▶[24.11.05 07:52] 02. 소스 올리기(by 진태만[PC 명: TAMA-MSI-A10SD-I7-21Y])

--- 90_etc/git_pull_push.py
+++ 90_etc/git_pull_push.py
@@ -17,8 +17,8 @@ sMsg2 = "[T_01] ■■■■■■  [######################### [01. git_pull_pus
 sMsg = "\n\n\n ■■■ [/git_pull_push.py] ==> "
 print(sMsg + sMsg2)
 
-now_ydmhm = time.strftime("%y.%m.%d %H:%M")	    # 오늘 날짜(년.월.일 시:분)
-git_way = "".join(map(str.lower, sys.argv[1:]))  # g 명령줄 인자 출력[git 방식(get, push)] ==> sys.argv[1][1 :]
+now_ydmhm = time.strftime("%y.%m.%d %H:%M")     # 오늘 날짜(년.월.일 시:분)
+git_way = "".join(map(str.lower, sys.argv[1:]))  # 명령줄 인자 출력[git 방식(get, push)] ==> sys.argv[1][1 :]
 
 command = 'git pull origin main'    # 1. ghtHub에서 소스 받아 오기
 proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
@@ -30,17 +30,17 @@ if str(out_cmd.decode('utf-8')) == "Already up to date.\n":   # 인자값이 pus
     git_way_no =  int(git_way_no) - 1
 print("[@_T] ■■■ [/git_pull_push.py] ==> [T_42] [git_way]"+ str(git_way) +"[git_way_no]"+ str(git_way_no) )
 
-# if (git_way == None or git_way == '' ) and git_way_no == 0:
-#     rsltFileNmAlert = "1. ghtHub에서 소스 받아 오기 실패 했습니다.(변경 사항 없음)"
-#     result = pyautogui.alert(rsltFileNmAlert, title='▶ [pull 확인 결과]', button='OK')
-#     sys.exit()    # 종료
+if (git_way == None or git_way == '' ) and git_way_no == 0:
+    rsltFileNmAlert = "1. ghtHub에서 소스 받아 오기 실패 했습니다.(변경 사항 없음)"
+    result = pyautogui.alert(rsltFileNmAlert, title='▶ [pull 확인 결과]', button='OK')
+    sys.exit()    # 종료
 
 if git_way == None or git_way == '':
-    # command = 'git log --oneline --all --graph'   # 로그 보기
-    # proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-    # out_cmd = proc.read()
+    command = 'git log --oneline --all --graph'   # 로그 보기
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+    out_cmd = proc.read()
 
-    git_way = "push"   # 인자값이 push 이면(ghtHub에 올리기) ===> TEST @@@@ ===>
+# git_way = "push"   # 인자값이 push 이면(ghtHub에 올리기) ===> TEST @@@@ ===>
 # result = pyautogui.alert(git_way, title='▶ [git_way 확인]', button='OK')
 # print("[@_T] ■■■ [/git_pull_push.py] ==> [T_44]")
 
@@ -61,11 +61,11 @@ if str(git_way) == "push":   # 인자값이 push 이면(ghtHub에 올리기)
     print("[@_T] ■■■ [/git_pull_push.py] ==> [T_71_1] [out_cmd_status]"+ out_cmd_status.decode('utf-8') )
 
     srch_word_1 = "nothing to commit"    # 조회 단어1
-    srch_word_2 = "no changes added to commit"    # 조회 단어2
+    # srch_word_2 = "no changes added to commit"    # 조회 단어2
     out_cmd_status_cd_1 = out_cmd_status.decode('utf-8').find(srch_word_1)  # 조회 단어1이 처음 나타나는 위치
-    out_cmd_status_cd_2 = out_cmd_status.decode('utf-8').find(srch_word_2)  # 조회 단어2이 처음 나타나는 위치
+    # out_cmd_status_cd_2 = out_cmd_status.decode('utf-8').find(srch_word_2)  # 조회 단어2이 처음 나타나는 위치
     # print(out_cmd_status.find('nothing to commit'))     # nothing to commit가 처음 나타나는 위치
-    print("[@_T] ■■■ [/git_pull_push.py] ==> [T_71_2] [out_cmd_status_cd_1]"+ str(out_cmd_status_cd_1) +"[out_cmd_status_cd_2]"+ str(out_cmd_status_cd_2) )
+    print("[@_T] ■■■ [/git_pull_push.py] ==> [T_71_2] [조회 단어1이 처음 나타나는 위치]"+ str(out_cmd_status_cd_1) )
 
     if out_cmd_status_cd_1 < 0:  # 조회 단어1가 없으면
     # if out_cmd_status_cd_1 > 0 and out_cmd_status_cd_2 > 0:
@@ -88,7 +88,7 @@ if str(git_way) == "push":   # 인자값이 push 이면(ghtHub에 올리기)
         print("[@_T] ■■■ [/git_pull_push py] ==> [T_74] [5. 원격 저장소에 반영(git push)]"+ out_cmd_push.decode('utf-8'))
         print("[@_T] ■■■ [/git_pull_push.py] ==> [T_80] [Git 정보 처리 시작 End] ■■■ @@@@@ ■■■■■■■■■ ■\n\n")
     else:
-        rsltFileNmAlert = "2. 커밋할 소스가 없습니다.(변경 사항 없음)"
+        rsltFileNmAlert = "2. 커밋할 소스가 없습니다.(수정한 내용 없음)"
         result = pyautogui.alert(rsltFileNmAlert, title='▶ [pull 확인 결과]', button='OK')
         sys.exit()    # 종료
 
