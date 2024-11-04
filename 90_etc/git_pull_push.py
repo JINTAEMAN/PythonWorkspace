50e913df95cbe0824947286e94a9aa0ea4bb8821
tamario <tamario@naver.com>
2024-11-05 07:43:22 +0900

    ▶[24.11.05 07:43] 02. 소스 올리기(by 진태만[PC 명: TAMA-MSI-A10SD-I7-21Y])

--- 90_etc/git_pull_push.py
+++ 90_etc/git_pull_push.py
@@ -1,8 +1,8 @@
 # ! /git_pull_push.py   # python Workspace 처리 파일
 # -*- coding: utf-8 -*-	# 문자 인코딩(한글 사용)
 
-import os	# 운영체제(OS) 제어 Lib 
-import platform  # platform 정보 Lib 
+import os	# 운영체제(OS) 제어 Lib
+import platform  # platform 정보 Lib
 import sys	# 파이씬 인터프리터 제어 Lib
 import csv	# CSV 파일 Lib
 import shutil      # 고수준 파일 연산 Lib
@@ -30,17 +30,17 @@ if str(out_cmd.decode('utf-8')) == "Already up to date.\n":   # 인자값이 pus
     git_way_no =  int(git_way_no) - 1
 print("[@_T] ■■■ [/git_pull_push.py] ==> [T_42] [git_way]"+ str(git_way) +"[git_way_no]"+ str(git_way_no) )
 
-if (git_way == None or git_way == '' ) and git_way_no == 0:
-    rsltFileNmAlert = "1. ghtHub에서 소스 받아 오기 실패 했습니다(변경 사항 없음)"
-    result = pyautogui.alert(rsltFileNmAlert, title='▶ [pull 확인 결과]', button='OK')
-    sys.exit()    # 종료
+# if (git_way == None or git_way == '' ) and git_way_no == 0:
+#     rsltFileNmAlert = "1. ghtHub에서 소스 받아 오기 실패 했습니다.(변경 사항 없음)"
+#     result = pyautogui.alert(rsltFileNmAlert, title='▶ [pull 확인 결과]', button='OK')
+#     sys.exit()    # 종료
 
 if git_way == None or git_way == '':
-    command = 'git log --oneline --all --graph'   # 로그 보기
-    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-    out_cmd = proc.read()
+    # command = 'git log --oneline --all --graph'   # 로그 보기
+    # proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+    # out_cmd = proc.read()
 
-# git_way = "push"   # 인자값이 push 이면(ghtHub에 올리기) ===> TEST @@@@ ===>
+    git_way = "push"   # 인자값이 push 이면(ghtHub에 올리기) ===> TEST @@@@ ===>
 # result = pyautogui.alert(git_way, title='▶ [git_way 확인]', button='OK')
 # print("[@_T] ■■■ [/git_pull_push.py] ==> [T_44]")
 
@@ -48,39 +48,48 @@ sMsg2 = "[T_42_0] [git 방식(인자값)]"+ str(git_way) +"\n"
 sMsg2 += "[0. 로그 보기]  ■■■■■■■■■■ \n"
 sMsg2 += ""+ out_cmd.decode('utf-8') +"\n"
 print(sMsg + sMsg2)
-print("[@_T] ■■■ [/git_pull_push.py] ==> [T_44] [git_way]"+ str(git_way) +"[git_way_no]"+ str(git_way_no) )
 
 my_host_nm = platform.uname().node  # 컴퓨터 현재 사용자 이름 가져오기(PC 명)
-print("[@_T] ■■■ [/git_pull_push.py] ==> [T_44] [PC 명]"+ str(my_host_nm) )
+print("[@_T] ■■■ [/git_pull_push.py] ==> [T_50] [PC 명]"+ str(my_host_nm) +"[git 방식]"+ str(git_way) +"[git 방식 No]"+ str(git_way_no) )
 
 if str(git_way) == "push":   # 인자값이 push 이면(ghtHub에 올리기)
-    print("[@_T] ■■■ [/git_pull_push.py] ==> [T_51]")
-
     command = 'git status'    # 0. Git 저장소의 상태 확인
 
     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
     out_cmd_status = proc.read()
     print("[@_T] ■■■ [/git_pull_push.py] ==> [T_71] [0. Git 저장소의 상태 확인(git status)] ■■■■■ ★★★★★ ■■■■■■■ "+ out_cmd_status.decode('utf-8') )
+    print("[@_T] ■■■ [/git_pull_push.py] ==> [T_71_1] [out_cmd_status]"+ out_cmd_status.decode('utf-8') )
 
-    command = 'git add -A'    # 3. Git 스테이지 영역에 추가
-    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-    out_cmd_add = proc.read()
-    print("[@_T] ■■■ [/git_pull_push.py] ==> [T_72] [3. Git 스테이지 영역에 추가(git add)]"+ out_cmd_add.decode('utf-8') )
+    srch_word_1 = "nothing to commit"    # 조회 단어1
+    srch_word_2 = "no changes added to commit"    # 조회 단어2
+    out_cmd_status_cd_1 = out_cmd_status.decode('utf-8').find(srch_word_1)  # 조회 단어1이 처음 나타나는 위치
+    out_cmd_status_cd_2 = out_cmd_status.decode('utf-8').find(srch_word_2)  # 조회 단어2이 처음 나타나는 위치
+    # print(out_cmd_status.find('nothing to commit'))     # nothing to commit가 처음 나타나는 위치
+    print("[@_T] ■■■ [/git_pull_push.py] ==> [T_71_2] [out_cmd_status_cd_1]"+ str(out_cmd_status_cd_1) +"[out_cmd_status_cd_2]"+ str(out_cmd_status_cd_2) )
 
-    cmd_cmt_msg = 'git commit -m "▶['+ now_ydmhm +'] 02. 소스 올리기(by 진태만[PC 명: '+ my_host_nm +'])"'   # 4. Git 로컬 저장소 영역에 커밋 처리
-    # git commit -m ' [TM] Add to Python Work space[23.08.26, by 진태만]'
-    proc = subprocess.Popen(cmd_cmt_msg, shell=True, stdout=subprocess.PIPE).stdout
-    out_cmt_msg = proc.read()
-    print("[@_T] ■■■ [/git_pull_push.py] ==> [T_73] [4. Git 로걸 저장소 영역에 추가(커밋 메세지)] ■■■■■■ ★★ ■■■■■■"+ str(cmd_cmt_msg) +"\n\n")
-    # print("[@_T] ■■■ [/git_pull_push.py] ==> [T_73_2] [4  Git 로걸 저장소 영역에 추가(git commit)] ■■■■■■■■■■■■ "* out_cmt_msg.decode('utf-8') )
+    if out_cmd_status_cd_1 > 0 or out_cmd_status_cd_2 > 0:
+        command = 'git add -A'    # 3. Git 스테이지 영역에 추가
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+        out_cmd_add = proc.read()
+        print("[@_T] ■■■ [/git_pull_push.py] ==> [T_72] [3. Git 스테이지 영역에 추가(git add)]"+ out_cmd_add.decode('utf-8') )
 
-    command = 'git push origin main'    # 5. ghtHub에 소스 올리기
-    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
-    out_cmd_push = proc.read()
-    git_way_no = int(git_way_no) + 3    # git 방식 No(1: pull: ghtHub에서 소스 받아 오기, 2: push: ghtHub에 소스 올리기, 3: 1 + 2)
-    print("[@_T] ■■■ [/git_pull_push py] ==> [T_74] [5. 원격 저장소에 반영(git push)]"+ out_cmd_push.decode('utf-8'))
-    print("[@_T] ■■■ [/git_pull_push.py] ==> [T_80] [Git 정보 처리 시작 End] ■■■ @@@@@ ■■■■■■■■■ ■\n\n")
-    print("[@_T] ■■■ [/git_pull_push.py] ==> [T_81] ■■■■■■[git_way_no]"+ str(git_way_no) )
+        cmd_cmt_msg = 'git commit -m "▶['+ now_ydmhm +'] 02. 소스 올리기(by 진태만[PC 명: '+ my_host_nm +'])"'   # 4. Git 로컬 저장소 영역에 커밋 처리
+        # git commit -m ' [TM] Add to Python Work space[23.08.26, by 진태만]'
+        proc = subprocess.Popen(cmd_cmt_msg, shell=True, stdout=subprocess.PIPE).stdout
+        out_cmt_msg = proc.read()
+        print("[@_T] ■■■ [/git_pull_push.py] ==> [T_73] [4. Git 로걸 저장소 영역에 추가(커밋 메세지)] ■■■■■■ ★★ ■■■■■■"+ str(cmd_cmt_msg) +"\n\n")
+        # print("[@_T] ■■■ [/git_pull_push.py] ==> [T_73_2] [4  Git 로걸 저장소 영역에 추가(git commit)] ■■■■■■■■■■■■ "* out_cmt_msg.decode('utf-8') )
+
+        command = 'git push origin main'    # 5. ghtHub에 소스 올리기
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout
+        out_cmd_push = proc.read()
+        git_way_no = int(git_way_no) + 3    # git 방식 No(1: pull: ghtHub에서 소스 받아 오기, 2: push: ghtHub에 소스 올리기, 3: 1 + 2)
+        print("[@_T] ■■■ [/git_pull_push py] ==> [T_74] [5. 원격 저장소에 반영(git push)]"+ out_cmd_push.decode('utf-8'))
+        print("[@_T] ■■■ [/git_pull_push.py] ==> [T_80] [Git 정보 처리 시작 End] ■■■ @@@@@ ■■■■■■■■■ ■\n\n")
+    else:
+        rsltFileNmAlert = "2. 커밋할 소스가 없습니다.(변경 사항 없음)"
+        result = pyautogui.alert(rsltFileNmAlert, title='▶ [pull 확인 결과]', button='OK')
+        sys.exit()    # 종료
 
 print("[@_T] ■■■ [/git_pull_push.py] ==> [T_90] ■■■■■■[git_way_no]"+ str(git_way_no) )
 
