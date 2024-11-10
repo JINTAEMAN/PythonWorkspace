77058fd1e54dbb75a127783ab3f6963155218aca
tamario <tamario@naver.com>
2024-11-11 07:20:21 +0900

    ▶[24.11.11 07:20] 02. 소스 올리기(by 진태만[PC 명: TAMA-MSI-A10SD-I7-21Y])

--- 90_etc/91. 변경 소스_■/01. GIt_소스 받아 오기■.bat
+++
@@ -1,3 +0,0 @@
-REM 01. GIt_소스 받아 오기■
-REM "C:\Python311\python.exe" "D:\PythonWorkspace\90_etc\git_pull_push.py"
-python git_pull_push.py
--- 90_etc/91. 변경 소스_■/02. GIt_소스 올리기■.bat
+++
@@ -1,2 +0,0 @@
-REM 02. GIt_소스 올리기■
-python git_pull_push.py push
